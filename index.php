4a5621a32350a34d7ae578f90eac064fb5d2c9a8
zhoubin <jobn_joy@163.com>
2017-01-04 21:40:52 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>欢迎学习php!</title>
+    </head>
+	<body>
+       <p>
+       <?php
+        echo 'php学到家，学到家虚';
+       ?>
+     </p>
+
+	</body>
+</html>
+
+<!-- 可以把两个字符用连接符连接起来，像这样:
+
+<?php echo 'Hi,'.'imooc!';?> -->
